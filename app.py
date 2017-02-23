8982cd24cc8e3365c1fb5ec740a7df20985910c7
Aly Sivji <alysivji@gmail.com>
2017-02-23 08:58:50 -0600

    Set up initial connect to Mongo, load last run date

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python3
+
 from flask import Flask
+from mongoengine.connection import connect
+from data_model import Post
+
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def getDate():
+    # connect to db
+    MONGO_URI = 'mongodb://localhost:27017'
+    connect('sivji-sandbox', host=MONGO_URI)
+
+    ## get the last date the webscraper was run
+    for post in Post.objects().fields(date=1).order_by('-date').limit(1):
+        day_to_pull = post.date.date()
+
+    return '{0}'.format(day_to_pull)
 
 if __name__ == "__main__":
     app.run()
