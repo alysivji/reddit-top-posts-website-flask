3e62541a25cc412e976ee9948e92d3fdd4604922
Aly Sivji <alysivji@gmail.com>
2017-02-23 10:37:11 -0600

    Load data from Mongo into basic jinja2 template

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 from mongoengine.connection import connect
 from data_model import Post
 
@@ -8,7 +8,7 @@ from data_model import Post
 app = Flask(__name__)
 
 @app.route("/")
-def getDate():
+def get_top_posts():
     # connect to db
     MONGO_URI = 'mongodb://localhost:27017'
     connect('sivji-sandbox', host=MONGO_URI)
@@ -17,7 +17,11 @@ def getDate():
     for post in Post.objects().fields(date=1).order_by('-date').limit(1):
         day_to_pull = post.date.date()
 
-    return '{0}'.format(day_to_pull)
+    # return render_template('template.html', Post=Post, day_to_pull=day_to_pull)
+    return render_template(
+        'template.html',
+        Post=Post,
+        day_to_pull=day_to_pull)
 
 if __name__ == "__main__":
     app.run()
