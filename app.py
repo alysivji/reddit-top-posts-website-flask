a5f22ea88c09fe9644bddbabcc913e5b32824f10
Aly Sivji <alysivji@gmail.com>
2017-02-24 11:27:57 -0600

    Create jinja2 website templates and populate with data from last scraper run

--- app.py
+++ app.py
@@ -2,26 +2,30 @@
 
 from flask import Flask, render_template
 from flask_mongoengine import MongoEngine
+from models import db, Post
 
 # this will change once we start using app factories
 # http://docs.mongoengine.org/projects/flask-mongoengine/en/latest/
 app = Flask(__name__)
+
 ## include db name in URI; it overwrites all others
 app.config['MONGODB_HOST'] = 'mongodb://localhost:27017/sivji-sandbox'
+app.debug = True
 
-
-from models import db, Post
+# initalize app with database
 db.init_app(app)
 
+
 @app.route("/")
-def get_top_posts():
+def index():
     ## get the last date the webscraper was run
     for post in Post.objects().fields(date=1).order_by('-date').limit(1):
         day_to_pull = post.date.date()
 
-    # return render_template('template.html', Post=Post, day_to_pull=day_to_pull)
+    print (type(day_to_pull))
+
     return render_template(
-        'template.html',
+        'index.html',
         Post=Post,
         day_to_pull=day_to_pull)
 
