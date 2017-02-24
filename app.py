2b3eb6b1a495c7701394e9a4bc613449db60a991
Aly Sivji <alysivji@gmail.com>
2017-02-23 18:39:37 -0600

    Refactored data model into separate module

--- app.py
+++ app.py
@@ -8,24 +8,10 @@ from flask_mongoengine import MongoEngine
 app = Flask(__name__)
 ## include db name in URI; it overwrites all others
 app.config['MONGODB_HOST'] = 'mongodb://localhost:27017/sivji-sandbox'
-db = MongoEngine()
+
+
+from models import db, Post
 db.init_app(app)
-
-class Post(db.Document):
-    ''' Class for defining structure of reddit-top-posts collection
-    '''
-    url = db.URLField(required=True)
-    date = db.DateTimeField(required=True)
-    commentsUrl = db.URLField(required=True)
-    sub = db.StringField(max_length=20, required=True) # subredit can be 20 chars
-    title = db.StringField(max_length=300, required=True) # title can be 300 chars
-    score = db.IntField(required=True)
-
-    meta = {
-        'collection': 'top_reddit_posts', # collection name
-        'ordering': ['-score'], # default ordering
-        'auto_create_index': False, # MongoEngine will not create index
-        }
 
 @app.route("/")
 def get_top_posts():
---
+++ models.py
@@ -0,0 +1,20 @@
+from flask_mongoengine import MongoEngine
+
+db = MongoEngine()
+
+
+class Post(db.Document):
+    ''' Class for defining structure of reddit-top-posts collection
+    '''
+    url = db.URLField(required=True)
+    date = db.DateTimeField(required=True)
+    commentsUrl = db.URLField(required=True)
+    sub = db.StringField(max_length=20, required=True) # subredit can be 20 chars
+    title = db.StringField(max_length=300, required=True) # title can be 300 chars
+    score = db.IntField(required=True)
+
+    meta = {
+        'collection': 'top_reddit_posts', # collection name
+        'ordering': ['-score'], # default ordering
+        'auto_create_index': False, # MongoEngine will not create index
+        }
