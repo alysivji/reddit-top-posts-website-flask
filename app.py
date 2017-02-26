d69ea68c35af3cdde13dc3e0f4e07767b54a4133
Aly Sivji <alysivji@gmail.com>
2017-02-26 16:06:55 -0600

    Refactor controller logic to separate out functionality

--- app.py
+++ app.py
@@ -15,12 +15,36 @@ app.debug = True
 # initalize app with database
 db.init_app(app)
 
+def get_last_rundate():
+    """Gets the last run date from the database
+
+    Arg:
+        None
+
+    Returns:
+        date_str field (format: 'YYYY-MM-DD')
+    """
+    # run a query to grab the last post
+    for post in Post.objects().fields(date_str=1).order_by('-date_str').limit(1):
+        return post.date_str
+
+def posts_get_distinct_items(field):
+    """Get list of distinct items contained in the Post collection
+
+    Arg:
+        field: field name we want distinct items for
+
+    Returns:
+        list of distinct items
+    """
+    # pass in as keyword arguments
+    params = {field: 1}
+    return list(Post.objects().fields(**params).distinct(field))
+
 
 @app.route("/")
 def index():
-    ## get the last date the webscraper was run
-    for post in Post.objects().fields(date_str=1).order_by('-date_str').limit(1):
-        day_to_pull = post.date_str
+    day_to_pull = get_last_rundate()
 
     return render_template(
         'index.html',
@@ -28,17 +52,14 @@ def index():
         day_to_pull=day_to_pull
         )
 
-
 @app.route("/date")
 def all_dates():
-    ## get all the dates the scraper was run on
-    dates = Post.objects().fields(date_str=1).distinct('date_str')
+    dates = posts_get_distinct_items('date_str')
 
     return render_template(
         'all-dates.html',
-        dates=reversed(list(dates)) # latest date on top
+        dates=reversed(dates) # latest date on top
         )
-
 
 @app.route("/date/<day_to_pull>")
 def by_date(day_to_pull=None):
@@ -48,17 +69,14 @@ def by_date(day_to_pull=None):
         day_to_pull=day_to_pull
         )
 
-
 @app.route("/sub")
 def all_subs():
-    ## get all the dates the scraper was run on
-    subs = Post.objects().fields(sub=1).distinct('sub')
+    subs = posts_get_distinct_items('sub')
 
     return render_template(
         'all-subreddits.html',
-        subs=sorted(list(subs), key=str.lower) # sort list of subreddits
+        subs=sorted(subs, key=str.lower) # sort list of subreddits
         )
-
 
 @app.route("/sub/<sub_to_pull>")
 def by_subreddit(sub_to_pull=None):
